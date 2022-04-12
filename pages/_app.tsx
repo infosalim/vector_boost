516935ca5fbf1e18bbf2168efda4bc1406c30f21
Salim Hossain <salimh5757@gmail.com>
2022-04-13 05:36:13 +0600

    dir change

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import Nav from './components/Nav'
+import Nav from '../components/Nav'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return  <>
